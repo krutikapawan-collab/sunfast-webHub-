e86b2ba2a868190c3a1a3305fa050160f9670b83
krutikapawan-collab <krutikapawan@gmail.com>
2026-08-02 12:47:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -202,3 +202,38 @@ navigator.clipboard.writeText(pass.value);
 alert("Password Copied!");
 
 }
+// ================= AGE CALCULATOR =================
+
+function calculateAge() {
+
+const dob = document.getElementById("dob").value;
+
+if (!dob) {
+    alert("Please select your Date of Birth.");
+    return;
+}
+
+const birthDate = new Date(dob);
+const today = new Date();
+
+let years = today.getFullYear() - birthDate.getFullYear();
+let months = today.getMonth() - birthDate.getMonth();
+let days = today.getDate() - birthDate.getDate();
+
+if (days < 0) {
+    months--;
+    const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    days += lastMonth.getDate();
+}
+
+if (months < 0) {
+    years--;
+    months += 12;
+}
+
+document.getElementById("ageResult").innerHTML =
+    years + " Years " +
+    months + " Months " +
+    days + " Days";
+
+}
